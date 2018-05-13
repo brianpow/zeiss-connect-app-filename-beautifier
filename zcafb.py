1b84689ed32cf4c7e4d4f7958f583b9faf89ccd6
Brian Pow <brianpow@gmail.com>
2018-05-13 22:23:51 +0800

    change rename behaviour to allow more than one patient with the same name

--- zcafb.py
+++ zcafb.py
@@ -22,94 +22,67 @@ parser.add_argument(
     '-V',
     '--version',
     action='version',
-    version='%(prog)s 1.0')
+    version='%(prog)s 1.1')
 
 args = parser.parse_args()
 
 
-def rename_folder(c, undo):
-    print "Renaming folders..."
+def get_folders(c, undo):
+    paths=[]
     for row in c.execute('SELECT ZPATIENTID, ZFIRSTNAME, ZLASTNAME, ZGENDER, strftime("%Y-%m-%d",ZDOB), ZDOB, strftime("%Y-%m-%d",\'now\') FROM ZPATIENTINFO'):
         original_path=row[0]
         human_path=", ".join([row[2],row[1],row[3], "(" + time.strftime("%Y-%m-%d", time.localtime(row[5]+978307200)) + ")",row[0]])
-        if undo:
-            src = os.sep.join(
-                [path, human_path])
-
-            dst = os.sep.join([path, original_path])
-            if args.archive:
-                src1 = os.sep.join(
-                    [args.archive,human_path])
-                dst1 = os.sep.join([args.archive, original_path])
-
-        else:
-            dst = os.sep.join(
-                [path, human_path])
-            src = os.sep.join([path, original_path])
-            if args.archive:
-                dst1 = os.sep.join(
-                    [args.archive, human_path])
-                src1 = os.sep.join([args.archive, original_path])
-
-        if args.verbose:
-            print "Renaming folder %s to %s" % (src, dst)
-            if args.archive:
-                print "Renaming folder %s to %s" % (src1, dst1)
-        try:
-            if not args.dryrun:
-                os.rename(src, dst)
-        except OSError:
-            print "Error occured when renaming %s to %s" % (src, dst)
-
-        try:
-            if not args.dryrun:
-                if args.archive:
-                    os.rename(src1, dst1)
-        except OSError:
-            print "Error occured when renaming %s to %s" % (src, dst)
-
-    return
+        paths.append((original_path,human_path))
+    return paths
 
 
 def rename_files(c, undo):
-    # print not args.undo
-    if args.undo:
-        rename_folder(conn.cursor(), args.undo)
-
+    paths_dictionaries=get_folders(conn.cursor(), args.undo)
+    full_paths_dictionaries = []
     print "Renaming files..."
-    # print args.undo
+    
     for row in c.execute('SELECT * FROM ZVISITRECORDS'):
-        file_path = row[16].split("/")[-2:]
-        extension = file_path[1].split(".")[-1:][0]
-        relative_path = file_path[0]
-        original_name = file_path[1]
+        old_full_path_with_filename = row[18].split("/")[-2:]
+        extension = old_full_path_with_filename[1].split(".")[-1:][0]
+        old_relative_path = old_full_path_with_filename[0]
+        old_filename = old_full_path_with_filename[1]
+        new_relative_path = ""
+        
+        for key,value in paths_dictionaries:
+            if key == old_relative_path:
+                new_relative_path = value
+                break
+        
         t = time.localtime(
             row[14] + datetime.timedelta(365 * 31 + 8).total_seconds())
-        microseconds = str(row[14]).split(".")[1]
-        new_name = os.sep.join(
+        microseconds = str(row[15]).split(".")[1]
+        new_filename = os.sep.join(
             ["%s %s.%s" % (time.strftime("%Y-%m-%d %H-%M-%S", t), microseconds, extension)])
+            
         if row[4]:
             if not args.archive:
                 continue
-            final_path = os.sep.join([args.archive, relative_path])
+            old_full_path = os.sep.join([args.archive, old_relative_path])
+            new_full_path = os.sep.join([args.archive, new_relative_path])
         else:
-            final_path = os.sep.join([path, relative_path])
-        if args.undo:
-            src = os.sep.join([final_path, new_name])
-            dst = os.sep.join([final_path, original_name])
-        else:
-            dst = os.sep.join([final_path, new_name])
-            src = os.sep.join([final_path, original_name])
+            old_full_path = os.sep.join([path, old_relative_path])
+            new_full_path = os.sep.join([path, new_relative_path])
 
+        if args.undo:
+            src = os.sep.join([new_full_path, new_filename])
+            dst = os.sep.join([old_full_path, old_filename])
+        else:
+            dst = os.sep.join([new_full_path, new_filename])
+            src = os.sep.join([old_full_path, old_filename])
+        
+        full_paths_dictionaries.append((src,dst))
         if args.verbose:
             print "Renaming %s to %s" % (src, dst)
         try:
             if not args.dryrun:
-                os.rename(src, dst)
+                os.renames(src, dst)
         except OSError:
             print "Error occured when renaming %s to %s" % (src, dst)
-    if not args.undo:
-        rename_folder(conn.cursor(), args.undo)
 
 
 for path in args.paths:
